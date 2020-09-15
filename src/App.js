3ea7a7231a9ced65c9c191ccd827c7c8a0fec702
Kien Hoang <kienhoang1891997@gmail.com>
2020-09-15 21:39:23 +0700

    remove uuid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-import uuid from "uuid";
+
 import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
